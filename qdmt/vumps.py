5fd23c9f9151f7be1cec82227ec1bed4aed6ea26
Vinul W <vinul25@live.co.uk>
2023-04-19 15:27:01 +0100

    MISC - Completed code outline for vumps with intial code
    - Bugs present as the energy is not optimising. Write tests for the
      different portions of the code.

--- qdmt/vumps.py
+++ qdmt/vumps.py
@@ -4,8 +4,10 @@ from ncon import ncon
 
 from numpy.linalg import qr
 from scipy.sparse.linalg import eigs
+from scipy.linalg import polar
 
 import matplotlib.pyplot as plt
+from hamiltonian import Hamiltonian
 
 def random_mixed_gauge(d, D):
     '''
@@ -37,7 +39,6 @@ def evaluateEnergy(AL, AR, C, h):
     TODO: Extend this to an n-site operator
     '''
     h = h.reshape(2, 2, 2, 2)
-    C = np.diag(C)
 
     AC_L = ncon([AL, C], ((-1, -2, 1), (1, -3)))
 
@@ -52,19 +53,69 @@ def gs_vumps(h, d, D, tol=1e-5, maxiter=100):
     Perform vumps to optimise local hamiltonian h.
     '''
     AL, AR, C = random_mixed_gauge(d, D)
+    C = np.diag(C)
 
     δ = 1
     count = 0
+    energies = []
     while δ > tol and maxiter > count:
         e = evaluateEnergy(AL, AR, C, h)
+        energies.append(e)
+        print(f'Current energy : {e}')
 
         h_shifted = h - e*np.eye(h.shape[0])
+        h_shifted = h.reshape(d, d, d, d)
 
         LH = sumLeft(AL, h_shifted)
-        LR = sumRight(AR, h_shifted)
+        RH = sumRight(AR, h_shifted)
 
         # TODO: Implementation still not complete
-        count += 1
+
+        # Construct Ac′
+        AC = ncon([C, AR], ((-1, 1), (1, -2, -3)))
+        I = np.eye(D) # Create identity map
+        Id = np.eye(d)
+
+        AC_map = ncon([AL, AL.conj(), h_shifted, I],
+                        ((1, 2, -1), (1, 3, -4), (3, -5, 2, -2), (-3, -6)))
+        AC_map += ncon([I, h_shifted, AR, AR.conj()],
+                         ((-1, -4), (-5, 3, -2, 1), (-3, 1, 2), (-6, 3, 2)))
+        AC_map += ncon([LH, Id, I], ((-1, -4), (-2, -5), (-3, -6)))
+        AC_map += ncon([I, Id, RH], ((-1, -4), (-2, -5), (-3, -6)))
+
+        AC_map_reshaped = AC_map.reshape(d*D**2, d*D**2)
+
+        _, v = eigs(AC_map_reshaped, k=1, which='LM', v0=AC.reshape(-1))
+
+        AC_prime = v[:, 0]
+        AC_prime = AC_prime.reshape(D, d, D)
+
+        # Construct C′
+        C_map = ncon([AL, AL.conj(), h_shifted, AR, AR.conj()],
+                       ((1, 2, -1), (1, 3, -3), (3, 6, 2, 4), (-2, 4, 5),
+                           (-4, 6, 5)))
+        C_map += ncon([LH, I], ((-1, -3), (-2, -4)))
+        C_map += ncon([I, RH], ((-1, -3), (-2, -4)))
+
+        C_map_reshaped = C_map.reshape(D**2, D**2)
+        _, v = eigs(C_map_reshaped, k=1, which='LM', v0=C.reshape(-1))
+
+        C_prime = v[:, 0].reshape(D, D)
+
+        AL, AR, AC, C = minAcC(AC_prime, C_prime)
+
+        # Check convergence
+        H_AC = ncon([AC_map, AC], ((1, 2, 3, -1, -2, -3), (1, 2, 3)))
+        H_C = ncon([C_map, C], ((1, 2, -1, -2), (1, 2)))
+        AL_HC = ncon([AL, H_C], ((-1, -2, 1), (1, -3)))
+
+        δ =  np.linalg.norm(H_AC - AL_HC)
+        count += 1 # iteratre counter for maxiter
+
+
+    e = evaluateEnergy(AL, AR, C, h) # Calculate the final energy
+    energies.append(e)
+    return AL, AR, C, energies
 
 def sumLeft(AL, h, tol=1e-8):
     from scipy.sparse.linalg import bicgstab
@@ -120,7 +171,7 @@ def sumLeft(AL, h, tol=1e-8):
 
     map_Lh = ncon([E_pseudo, Lh], ((1, 2, -1, -2), (1, 2)))
 
-    print(np.allclose(map_Lh, Hl, atol=1e-5))
+    # print(np.allclose(map_Lh, Hl, atol=1e-5))
     # print(np.linalg.norm(map_Lh - Hl))
 
     return Lh
@@ -149,9 +200,26 @@ def sumRight(AR, h, tol=1e-8):
     E_psuedo = E_psuedo.reshape(D, D, D, D)
 
     map_Rh = ncon([E_psuedo, Rh], ((-1, -2, 1, 2), (1, 2)))
-    print(np.allclose(map_Rh, Hr))
+    # print(np.allclose(map_Rh, Hr))
 
     return Rh
+
+def minAcC(Ac, C):
+    Ul_Ac, Pl_Ac = polar(Ac.reshape(D*d, D), side='left')
+    Ul_C, Pl_C = polar(C, side='left')
+
+    Al = Ul_Ac @ (Ul_C.conj().T)
+    Al = Al.reshape(D, d, D)
+
+    Ur_Ac, Pr_Ac = polar(Ac.reshape(D*d, D), side='right')
+    Ur_C, Pr_C = polar(C, side='right')
+
+    Ar = Ur_Ac @ (Ur_C.conj().T)
+    Ar = Ar.reshape(D, d, D)
+
+    C_updated = Pl_C @ Ul_C
+    Ac_updated = (Pl_Ac @ Ul_Ac).reshape(D, d, D)
+    return Al, Ar, Ac_updated, C_updated
 
 def largest_evec_left(E, l0 = None):
     '''
@@ -182,15 +250,20 @@ if __name__=="__main__":
     d = 2
     D = 4
     AL, AR, C = random_mixed_gauge(d, D)
+    C = np.diag(C)
 
     H = Hamiltonian({'ZZ':-1, 'X':0.2}).to_matrix()
 
-    energy = evaluateEnergy(AL, AR, C, H)
-
-    print(energy)
 
     print('Checking sum Left...')
     sumLeft(AL, H)
 
     print('Checking sum right...')
     sumRight(AR, H)
+
+    energy = evaluateEnergy(AL, AR, C, H)
+    print('Trying vumps...')
+
+    _ , _, _, energies = gs_vumps(H, 2, 4, maxiter=500)
+    plt.plot(energies, 'x-')
+    plt.show()
