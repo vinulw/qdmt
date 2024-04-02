dc946c4347394cb056fbdf474dcf6576a43cdfaf
Vinul W <vinul25@live.co.uk>
2024-04-02 15:51:15 +0100

    Compare sampled thermal exp from Transverse Ising for begineers

--- notebooks/sample_thermal.py
+++ notebooks/sample_thermal.py
@@ -5,8 +5,18 @@ from qdmt.hamiltonian import TransverseIsing
 from qdmt.uMPSHelpers import rightFixedPoint
 from tqdm import tqdm
 from datetime import datetime
+from functools import reduce
+
+from numpy import kron, eye
 
 import matplotlib.pyplot as plt
+
+# define Paulis
+X = np.array([[0, 1], [1, 0]], dtype=complex)
+Y = 1j*np.array([[0, -1], [1, 0]], dtype=complex)
+Z = np.array([[1, 0], [0, -1]], dtype=complex)
+
+S = {'I': eye(2, dtype=complex), 'X': X, 'Y': Y, 'Z': Z}
 
 def sampleUnitaryTensors(shape, N):
     '''
@@ -79,6 +89,36 @@ def test_rightFixedPointNormalised():
     for ov in selfOverlaps:
         assert np.allclose(ov, 1+0j)
 
+def generateHamiltonianPeriodic(J, g, n):
+    '''
+    Generate n qubit TFIM Hamiltonian.
+    '''
+
+    h = np.zeros((2**n, 2**n)) + 0j
+
+    for i in range(n-1):
+        pString = ['I'] * n
+        pString[i] = 'Z'
+        pString[i+1] = 'Z'
+        hzz = reduce(kron, [S[j] for j in pString])
+
+        pString = ['I'] * n
+        pString[i] = 'X'
+        hxx = reduce(kron, [S[j] for j in pString])
+
+        h += -J * hzz - g * hxx
+
+    pString = ['I'] * n
+    pString[n-1] = 'Z'
+    pString[0] = 'Z'
+    hzz = reduce(kron, [S[j] for j in pString])
+
+    pString = ['I'] * n
+    pString[n-1] = 'X'
+    hxx += reduce(kron, [S[j] for j in pString])
+    h += -J * hzz - g*hxx
+    return h
+
 
 if __name__=="__main__":
     # # Sampling states
@@ -119,6 +159,9 @@ if __name__=="__main__":
     # save = True
     # now = datetime.now().strftime('%d%m%y_%H%M%S')
 
+    # J = 1.0
+    # g = 0.5
+
     # shape = (4, 2, 4)
     # N = 100000
     # print('Collecting thermal distributions...')
@@ -126,7 +169,8 @@ if __name__=="__main__":
     # As = sampleUnitaryTensors(shape, N)
 
     # # print('Preparing H...')
-    # H = TransverseIsing(J=1.0, g=0.2, n=2)
+    # # H = TransverseIsing(J=J, g=g, n=2)
+    # H = generateHamiltonianPeriodic(J=J, g=g, n=2)
     # print('Calculating expectations...')
     # expHs = np.array([expectationLeftCanonical(A, H) for A in tqdm(As)])
 
@@ -141,10 +185,11 @@ if __name__=="__main__":
     # breakpoint()
 
     # Analyse thermal expectation
-    prefix = 'data/120324_163721_' # 100 000 samples
+    prefix = 'data/020424_153954_' # 100 000 samples
     print(f'Analysing data from run: {prefix}')
     plt.rc('text', usetex=True)
     plt.rc('text.latex', preamble=r'\usepackage{physics}')
+    plt.rcParams['font.size'] = 18
     expHs = np.load(prefix + 'expHs.npy')
 
     Ns = [1000, 5000, 10000, 50000]
@@ -154,7 +199,8 @@ if __name__=="__main__":
         if len(expHs) < N:    # skip N if bigger than no samples
             continue
         nBetas = 50
-        betas = np.logspace(-1, 2, nBetas)
+        kTs = np.logspace(-3, 0, nBetas)
+        betas = 1/kTs
         expHTherm = np.zeros((nBetas, repeats), dtype=complex)
         for j in range(repeats):
             expHs_ = np.random.choice(expHs, size=N, replace=False)
@@ -164,15 +210,16 @@ if __name__=="__main__":
                 expBHs = np.exp(-beta*expHs_)
                 expHTherm[i][j] = np.sum(np.dot(expBHs, expHs_)) / np.sum(expBHs)
 
+        expHTherm = expHTherm / 2
         means = np.mean(np.real(expHTherm), axis=1)
         stds = np.std(np.real(expHTherm), axis=1)
 
         # print(f'Expectations real: ', np.allclose(np.imag(expHs), 0))
-        plt.errorbar(betas, means, yerr=stds,
+        plt.errorbar(kTs, means, yerr=stds,
                       fmt='x-', label=f'N: {N}', capsize=3.0)
 
-    plt.xlabel(r'$\beta$')
+    plt.xlabel(r'$k_BT$')
     plt.ylabel(r'$\expval{H_{T}}$')
-    plt.title(r'Estimating $\expval{H_T}(\beta)$ using boosting. $N_{samp} = 10^5$')
+    plt.title(r'Estimating $\expval{H_T}(k_BT)$ using boosting. $N_{samp} = 10^5$')
     plt.legend()
     plt.show()
